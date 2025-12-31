f98ad4bfb43a2526d2617353f9f89efdfbc4f99d
acoolvoice03-ops <acoolvoice03@gmail.com>
2025-12-31 21:37:27 +0530

    Update config.py
    Signed-off-by: acoolvoice03-ops <acoolvoice03@gmail.com>

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 class Config:
     def __init__(self):
-        self.API_ID = int(getenv("API_ID", 27806628))
-        self.API_HASH = getenv("API_HASH","25d88301e886b82826a525b7cf52e090")
+        self.API_ID = int(getenv("API_ID", 29198115))
+        self.API_HASH = getenv("API_HASH", "c95739ca773e2e35efe11b406b59af3b")
 
         self.BOT_TOKEN = getenv("BOT_TOKEN","8577192556:AAH_VGCGYNA_s-_NFXeyXMb12_J7UIGK4Og")
         self.MONGO_URL = getenv("MONGO_URL", "mongodb+srv://Bosshub:JMaff0WvazwNxKky@cluster0.l0xcoc1.mongodb.net/?appName=Cluster0")
@@ -18,7 +18,7 @@ class Config:
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 20))
         self.PLAYLIST_LIMIT = int(getenv("PLAYLIST_LIMIT", 20))
 
-        self.SESSION1 = getenv("SESSION", BQF1s6cAiH_ojYTnUQ1NsyFD1Ulhf7VfMSYaa0VbaHky0PDjO8UyRtV1shBhOMegzTsXXt-ooUZRaiiGvWYmmjalNyBYzNHQjAumEJYbCbyGRl5RJGJdSupmiQBa3dR34AnKDc2_fNzp6PGsWk-qKlzZT8DhaFAlxDZmaha0GwPjmm8aYZlle1UsyZLbM_vx14nmHJT8gyGMbmifTxXh-JpL3_AaRCbnW9jLh0ULf_CSURvd7lg8gc578_4H0KvwSBtIYuyohvmyRC2kKX0-vQCn7X_MfFzqsxrQvdi2yYNWW5WgmzQID91602_GqPQ1-DzSOx5f7wNBPAmqTQ_uvxO5Ww-fLgAAAAGx9uaUAA)
+        self.SESSION1 = getenv("SESSION", "BQF1s6cAiH_ojYTnUQ1NsyFD1Ulhf7VfMSYaa0VbaHky0PDjO8UyRtV1shBhOMegzTsXXt-ooUZRaiiGvWYmmjalNyBYzNHQjAumEJYbCbyGRl5RJGJdSupmiQBa3dR34AnKDc2_fNzp6PGsWk-qKlzZT8DhaFAlxDZmaha0GwPjmm8aYZlle1UsyZLbM_vx14nmHJT8gyGMbmifTxXh-JpL3_AaRCbnW9jLh0ULf_CSURvd7lg8gc578_4H0KvwSBtIYuyohvmyRC2kKX0-vQCn7X_MfFzqsxrQvdi2yYNWW5WgmzQID91602_GqPQ1-DzSOx5f7wNBPAmqTQ_uvxO5Ww-fLgAAAAGx9uaUAA")
         self.SESSION2 = getenv("SESSION2", None)
         self.SESSION3 = getenv("SESSION3", None)
 
